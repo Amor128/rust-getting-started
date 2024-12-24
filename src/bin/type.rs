d6676da7a5574ef58d2c989b847d41e9a9a6ea82
Ermao <yangshifeng19@qq.com>
2024-12-24 23:16:31 +0800

    temp:202412242316

--- src/bin/type.rs
+++ src/bin/type.rs
@@ -85,3 +85,62 @@ pub mod primitive_type {
         }
     }
 }
+
+pub mod sequence_type {
+    pub mod tuple_type {
+        /**
+        元组是一个将多个其他类型的值组合在一起的复合类型。元组可以包含多个不同类型的值，但是一旦声明了元组的长度，就不能再改变了。
+        元组的长度是固定的，一旦声明了元组的长度，就不能再改变了。
+         */
+        pub fn tuple_type() {
+            let x: (i32, f64, u8) = (500, 6.4, 1);
+            let (a, b, c) = x;
+            println!("a: {}, b: {}, c: {}", a, b, c);
+            println!("x.0: {}, x.1: {}, x.2: {}", x.0, x.1, x.2);
+        }
+
+        /**
+        元组的类型可以通过下标访问，下标从 0 开始。
+         */
+        pub fn tuple_type_index() {
+            let x: (i32, f64, u8) = (500, 6.4, 1);
+            println!("x.0: {}, x.1: {}, x.2: {}", x.0, x.1, x.2);
+        }
+    }
+
+    pub mod array_type {
+        /**
+        数组是一个固定长度的同类型元素集合。数组的长度是固定的，一旦声明了数组的长度，就不能再改变了。
+        数组的类型是 [T; N]，其中 T 是元素类型，N 是长度。
+         */
+        pub fn array_type() {
+            let a = [1, 2, 3, 4, 5];
+            let b: [i32; 5] = [1, 2, 3, 4, 5];
+            let c = [3; 5]; // 等价于 let c = [3, 3, 3, 3, 3];
+            println!("a: {:?}", a);
+            println!("b: {:?}", b);
+            println!("c: {:?}", c);
+        }
+
+        /**
+        数组的类型是 [T; N]，其中 T 是元素类型，N 是长度。数组的长度是固定的，一旦声明了数组的长度，就不能再改变了。
+        数组的类型是 [T; N]，其中 T 是元素类型，N 是长度。
+         */
+        pub fn array_type_index() {
+            let a = [1, 2, 3, 4, 5];
+            println!("a[0]: {}, a[1]: {}, a[2]: {}", a[0], a[1], a[2]);
+        }
+    }
+
+    /**
+    切片是对数组的引用，它包含了数组的引用和长度。切片的类型是 &[T]，其中 T 是元素类型。
+    切片的类型是 &[T]，其中 T 是元素类型。
+     */
+    pub mod slice_type {
+        fn print_slice(slice: &[i32]) {
+            for val in slice {
+                println!("{}", val);
+            }
+        }
+    }
+}
